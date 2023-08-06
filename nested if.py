a0638b4ecbe6034d0e065f173179c39f7bf648d6
Rahima Akter Ratry <115058821+rahimaRatry@users.noreply.github.com>
2023-08-06 10:40:50 +0600

    Add files via upload

---
+++ nested if.py
@@ -0,0 +1,12 @@
+a = int(input())
+if a<10:
+    if(a%2)==0:
+        print('less, yes')
+    else:
+        print('less, no')
+
+else:
+    if(a%3)==0:
+        print('greater, yes')
+    else:
+        print('greater, no')
